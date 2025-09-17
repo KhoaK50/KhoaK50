71a19ade022f2db92a3c307ff939fb6b7daf6e82
Nguyễn Trần Đăng Khoa <sc3.nguyentrandangkhoa@gmail.com>
2025-09-17 15:46:31 +0700

    Delete project_web/frontend-client/scale_vector.js

--- project_web/frontend-client/scale_vector.js
+++
@@ -1,7 +0,0 @@
-
-    function scaleVector() {
-      callAPI("scale_vector", {
-        v: JSON.parse(document.getElementById("v_scale").value),
-        scalar: parseFloat(document.getElementById("scalar").value)
-      }, "result_scale");
-    }
