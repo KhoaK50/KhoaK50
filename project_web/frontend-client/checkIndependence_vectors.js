459ed1efe402d2f3d6dfb9a379d793fca8e5459e
Nguyễn Trần Đăng Khoa <sc3.nguyentrandangkhoa@gmail.com>
2025-09-17 15:43:25 +0700

    Delete project_web/frontend-client/checkIndependence_vectors.js

--- project_web/frontend-client/checkIndependence_vectors.js
+++
@@ -1,6 +0,0 @@
-
-    function checkIndependence() {
-      callAPI("linear_independence", {
-        vectors: JSON.parse(document.getElementById("vectors_indep").value)
-      }, "result_indep");
-    }
