c4227cb678e1444da8f6ac25ac52ad887a548f0f
Nguyễn Trần Đăng Khoa <sc3.nguyentrandangkhoa@gmail.com>
2025-09-17 15:46:05 +0700

    Delete project_web/frontend-client/projection_vector.js

--- project_web/frontend-client/projection_vector.js
+++
@@ -1,7 +0,0 @@
-
-    function projection() {
-      callAPI("projection", {
-        v: JSON.parse(document.getElementById("v_proj").value),
-        u: JSON.parse(document.getElementById("u_proj").value)
-      }, "result_proj");
-    }
