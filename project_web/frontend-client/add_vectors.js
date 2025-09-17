54d5da5e742ba57cc3fae987f9abf8bd4ac377bf
Nguyễn Trần Đăng Khoa <sc3.nguyentrandangkhoa@gmail.com>
2025-09-17 15:42:47 +0700

    Delete project_web/frontend-client/add_vectors.js

--- project_web/frontend-client/add_vectors.js
+++
@@ -1,7 +0,0 @@
-
-    function addVectors() {
-      callAPI("add_vectors", {
-        v1: JSON.parse(document.getElementById("v1_add").value),
-        v2: JSON.parse(document.getElementById("v2_add").value)
-      }, "result_add");
-    }
