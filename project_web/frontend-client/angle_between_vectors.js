ade582a1f5d5aa74871131a437db0f0bb2ea5b0e
Nguyễn Trần Đăng Khoa <sc3.nguyentrandangkhoa@gmail.com>
2025-09-17 15:42:59 +0700

    Delete project_web/frontend-client/angle_between_vectors.js

--- project_web/frontend-client/angle_between_vectors.js
+++
@@ -1,7 +0,0 @@
-
-    function angleBetween() {
-      callAPI("angle_between", {
-        v1: JSON.parse(document.getElementById("v1_angle").value),
-        v2: JSON.parse(document.getElementById("v2_angle").value)
-      }, "result_angle");
-    }
