ec28a6f9ba3756bca31e92725d97dbf255c6c246
Nguyễn Trần Đăng Khoa <sc3.nguyentrandangkhoa@gmail.com>
2025-09-17 15:46:49 +0700

    Delete project_web/frontend-client/sub_vectors.js

--- project_web/frontend-client/sub_vectors.js
+++
@@ -1,7 +0,0 @@
-
-    function subVectors() {
-      callAPI("sub_vectors", {
-        v1: JSON.parse(document.getElementById("v1_sub").value),
-        v2: JSON.parse(document.getElementById("v2_sub").value)
-      }, "result_sub");
-    }
