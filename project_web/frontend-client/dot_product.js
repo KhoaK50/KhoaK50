773de9930c161f13ac71cfc4982cd73b6e8e6d3b
Nguyễn Trần Đăng Khoa <sc3.nguyentrandangkhoa@gmail.com>
2025-09-17 15:44:07 +0700

    Delete project_web/frontend-client/dot_product.js

--- project_web/frontend-client/dot_product.js
+++
@@ -1,7 +0,0 @@
-
-    function dotProduct() {
-      callAPI("dot_product", {
-        v1: JSON.parse(document.getElementById("v1_dot").value),
-        v2: JSON.parse(document.getElementById("v2_dot").value)
-      }, "result_dot");
-    }
