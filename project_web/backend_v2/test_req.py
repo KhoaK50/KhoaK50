edfd3607f649df69b9fa64c22e6601c5675531b4
Nguyễn Trần Đăng Khoa <sc3.nguyentrandangkhoa@gmail.com>
2026-08-21 10:30:32 +0700

    Chore: Delete more old patch and test scripts

--- project_web/backend_v2/test_req.py
+++
@@ -1,4 +0,0 @@
-import requests
-res = requests.get('https://visualization-rr5v.onrender.com/api/verify?token=d257740b1598dc4c2ea194c9bc8ad909a5a958f4', allow_redirects=False)
-print(res.status_code)
-print(res.headers.get('Location'))
